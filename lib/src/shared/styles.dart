9955585b715fe252c85703eecd99d713db8566e8
Elhadj Ndiaye <elou95@gmail.com>
2022-06-02 03:53:53 +0000

    styles setup

---
+++ lib/src/shared/styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+
+
+const TextStyle headingStyle = TextStyle(
+  fontSize: 34,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle headlineStyle = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle bodyStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle subheadingStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle captionStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
